49532ee4db2df9901dd19ee1c6f681b3d2af5ec6
Aaditya Sriram <asriram4@ncsu.edu>
2015-04-12 13:32:30 -0400

    Added Kafka Producer

--- app.js
+++ app.js
@@ -9,14 +9,21 @@ var port = 3000;
 server.listen(port);
 console.log("Server Socket started and listening on port : " + port);
 
+var kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    client = new kafka.Client(),
+    producer = new Producer(client);
+
+
 // Using 'Twit' node.js library to fetch twitter stream of tweets currently getting tweeted.
 var Twit = require('twit');
+var prod = new kafka.Producer(new kafka.Client());
 
 // Passing the Twitter Credentials stored as environment variables.
 var T = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
@@ -25,7 +32,10 @@ var stream = T.stream('statuses/filter', { track: ['love', 'hate'] });
 
 //Listening to 'tweet' event which sends callback whenever someone sends a tweet with the specified filter.
 stream.on('tweet', function(tweet){
-    console.log(tweet);
+    payloads = [{topic : "tweet_message", messages : JSON.stringify(tweet)}]
+    prod.send(payloads, function(err, data) {
+    	console.log("Sent to kafka");
+    });
 });
 
 // catch 404 and forward to error handler
